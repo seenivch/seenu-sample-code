f538cd15a910d013b42524ddc07609f6b53c8f61
Seenivasan Chandrasekaran <seenu.chandar@gmail.com>
2019-10-19 19:33:16 +0530

    refs #Entity test commit

---
+++ SpringProject/src/main/java/com/seenu/SpringProject/DaoTestEntity.java
@@ -0,0 +1,25 @@
+package com.seenu.SpringProject;
+
+public class DaoTestEntity {
+
+	
+	private String a ;
+	private String b ;
+
+
+	public String getB() {
+		return b;
+	}
+
+	public void setB(String b) {
+		this.b = b;
+	}
+
+	public String getA() {
+		return a;
+	}
+
+	public void setA(String a) {
+		this.a = a;
+	}
+}
